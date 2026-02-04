c946f773c536fac50ec264b1e782173a48dafd62
Rosa <rosamariatlax@gmail.com>
2026-02-03 19:01:47 -0600

    clase 3

--- condicionales.js
+++ condicionales.js
@@ -24,3 +24,24 @@ if (edad >= 18 && tieneINE){
 }else{
     console.log('No puedes votar')
 }
+
+let numero = prompt ('ingresa un numero');
+numero = parseInt(numero);
+
+//si queremos preguntar si es divisible o no entre 2
+
+if (numero % 2 === 0){
+    console.log('Es divisible entre 2')
+}else{
+    console.log('No es divisible entre 2')
+}
+
+let esPremium = false;
+let totalCompra = 1200;
+let esFinDeSemana = true;
+
+if (esPremium || (totalCompra > 1000 && esFinDeSemana)){
+    console.log('Descuento aplicado')
+}else{
+    console.log('No aplica el descuento')
+}
